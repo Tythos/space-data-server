941ea7cbbc02c598906001c85a18ca5b16119d52
tj <tj@digitalarsenal.io>
2022-10-03 15:49:30 -0400

    cleanup

--- lib/routes/spacedata/spacedata.ts
+++ lib/routes/spacedata/spacedata.ts
@@ -6,9 +6,13 @@ export default (req: Request, res: Response, next: Function) => {
     if (!req.params.standard) {
         res.send(Object.keys(standards));
     } else {
-        let tt = new (standards as unknown as any)[req.params.standard.toUpperCase()+"T"]();
-        console.log(tt);
-        res.send(`<div>${JSON.stringify(tt, null, 4)}</div><script>setTimeout(()=>window.location = window.location, 1000)</script>`);
+        const predicate = req.params.standard.toUpperCase() + "T";
+        let dClass: any;
+        if (dClass = standards[predicate as keyof typeof standards]) {
+            let tt = new dClass();
+            res.send(`<div>${JSON.stringify(tt, null, 4)}</div>`);
+        }
+        res.send(`<script>setTimeout(()=>window.location = window.location, 1000)</script>`);
     }
 
     next();
