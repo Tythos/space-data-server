bf202a20ea95b0d7fab0fa5c39146a6fb43309dd
tj <tj@digitalarsenal.io>
2022-10-25 17:13:10 -0400

    sometimes I hate typescript

--- lib/standards/standards.ts
+++ lib/standards/standards.ts
@@ -1,5 +1,5 @@
-export { SATCAT, SATCATT, SATCATCOLLECTION, SATCATCOLLECTIONT } from "@/lib/class/standards/SAT/main";
-export { OMM, OMMT, OMMCOLLECTION, OMMCOLLECTIONT } from "@/lib/class/standards/OMM/main";
-export { CDM, CDMT, CDMCOLLECTION, CDMCOLLECTIONT } from "@/lib/class/standards/CDM/main";
-export { OPM, OPMT, OPMCOLLECTION, OPMCOLLECTIONT } from "@/lib/class/standards/OPM/main";
-export { OEM, OEMT, OEMCOLLECTION, OEMCOLLECTIONT } from "@/lib/class/standards/OEM/main";
+export * as SATCAT_STANDARD from "@/lib/class/standards/SAT/main";
+export * as OMM_STANDARD from "@/lib/class/standards/OMM/main";
+export * as CDM_STANDARD from "@/lib/class/standards/CDM/main";
+export * as OPM_STANDARD from "@/lib/class/standards/OPM/main";
+export * as OEM_STANDARD from "@/lib/class/standards/OEM/main";
--- test/generate.test.ts
+++ test/generate.test.ts
@@ -7,18 +7,20 @@ import * as standards from "../lib/standards/standards";
 
 const filename = "./test/output/standards.sqlite";
 const sqlfilename = "./test/output/standards.sql";
+let knexConnection: any;
 
 beforeAll(async () => {
     await generateDatabase(standardsJSON, filename, sqlfilename);
+    knexConnection = knex({
+        client: 'better-sqlite3',
+        connection: { filename },
+        useNullAsDefault: true
+    });
 });
 
 describe('Test Generation', () => {
     test('Generate Database With Correct Tables', async () => {
-        var knexConnection = knex({
-            client: 'better-sqlite3',
-            connection: { filename },
-            useNullAsDefault: true
-        });
+
 
         for (let s = 0; s < standardsJSON.length; s++) {
             let tableName = refRootName(standardsJSON[s].$ref);
@@ -30,7 +32,27 @@ describe('Test Generation', () => {
 
 describe('Test Data Entry', () => {
     test('Enter Data For Each Data Type', async () => {
-        console.log(standards)
+        let standard: keyof typeof standards;
+        for (standard in standards) {
+            console.log(standard, ":");
+            for (let s = 0; s < standardsJSON.length; s++) {
+                let tableName = refRootName(standardsJSON[s].$ref);
+                let pClassName: keyof typeof standards = `${tableName}_STANDARD` as unknown as any;
+                let parentClass: any = standards[pClassName];
+                console.log(parentClass);
+                let cClassName: keyof typeof parentClass = `${tableName}COLLECTIONT`;
+                let standardCollection = new parentClass[cClassName];
+                console.log(cClassName, parentClass[cClassName]);
+            }
+
+
+            /*
+           let standardClass: string;
+            for (standardClass in standards[standard]) {
+                console.log(standardClass);
+            }*/
+
+        }
         expect(1).toEqual(1);
     })
 });
