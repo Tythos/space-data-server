b86a7c9f2f96abcf7e4f917adc0d109962b27e22
tj <tj@digitalarsenal.io>
2022-11-28 14:08:51 -0500

    update build

--- scripts/updateVersion.mjs
+++ scripts/updateVersion.mjs
@@ -2,7 +2,11 @@ import { readFileSync, writeFileSync } from "fs";
 let packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 let newVersion = Date.now();
-packageJson.version = `${packageJson.version.split("+")[0]}+${newVersion}`;
+let pVersionParts = packageJson.version.split("+");
+let pVersion = pVersionParts[0].split(".").map((n) => parseInt(n));
+pVersion[pVersion.length - 1] = pVersion[pVersion.length - 1] + 1;
+
+packageJson.version = `${pVersion.join(".")}+${newVersion}`;
 writeFileSync("./package.json", JSON.stringify(packageJson, null, 4));
 
 let serverSrc = readFileSync("./build/server.cjs", "utf-8");
