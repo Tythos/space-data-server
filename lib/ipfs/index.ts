88bbe9ce9a25a17d083b1b0f98f24d9aa752dc03
tj <tj@digitalarsenal.io>
2022-12-02 14:21:12 -0500

    IPFS server setup test

--- lib/ipfs/index.ts
+++ lib/ipfs/index.ts
@@ -1,22 +1,76 @@
-import { execSync } from "child_process";
+import { join } from "path";
+import { ChildProcess, exec } from 'node:child_process';
+import { KeyValueDataStructure } from "../class/utility/KeyValueDataStructure";
+import { mkdir, existsSync, rmdirSync, mkdirSync } from "fs";
+const ipfsPath = join(__dirname, "../../", "go-ipfs/");
 
 /* TODO
 - https://docs.ipfs.tech/how-to/command-line-quick-start/
 - Start IPFS service using `child_process`
+- ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/9001
+- ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 - Set environment variable: `export IPFS_PATH="{pwd}/go-ipfs"` (or wherever)
 - run `ipfs init`
 - Import keys using custom IPFS format
 - Add / remove pins
 - Add / remove peers
 */
-let ipfsd;
-export const startIPFS = async (): Promise<any> => {
 
+export interface IPFSController {
+    process: ChildProcess,
+    gatewayPort: Number,
+    apiPort: Number,
+    env: {
+        IPFS_PATH: string
+    },
+    api: Function
 }
-export const stopIPFS = async () => {
-    if (ipfsd) {
-    } else {
-        throw Error("IPFS Daemon has not been initialized.");
+
+export interface IPFSControllerCollection {
+    [key: string]: IPFSController;
+}
+
+let ipfsDaemons: IPFSControllerCollection = {};
+
+const api = async function (path: string, args: object) {
+    let apiCall = fetch(`http://127.0.0.1:${this.apiPort}/api/v0${path}`, { method: 'POST' });
+    let returnObj = await (await apiCall).json();
+    return returnObj;
+}
+
+export const startIPFS = async (gatewayPort: Number = 5001, apiPort: Number = 9001, folderPath: string = ""): Promise<any> => {
+
+    let IPFS_PATH = join(ipfsPath, folderPath.length ? folderPath : gatewayPort.toString());
+
+    if (!existsSync(IPFS_PATH)) {
+        mkdirSync(IPFS_PATH);
     }
 
+    const env = { IPFS_PATH };
+    const execPath = `${ipfsPath}ipfs`;
+    const cmds = [`${execPath} config Addresses.Gateway /ip4/0.0.0.0/tcp/${gatewayPort}`,
+    `${execPath} config Addresses.API /ip4/0.0.0.0/tcp/${apiPort}`,
+    `${execPath} daemon`];
+
+    ipfsDaemons[gatewayPort.toString()] = {
+        env,
+        process: await exec(
+            cmds.join("&&"),
+            { env }) as ChildProcess,
+        gatewayPort,
+        apiPort,
+        api
+    } as IPFSController;
+
+    return ipfsDaemons[gatewayPort.toString()];
 }
+
+export const stopIPFS = async (gatewayPort: Number) => {
+    if (!gatewayPort) {
+        throw Error(`Unknown IPFS Instance ${gatewayPort}`);
+    } else {
+        return ipfsDaemons[gatewayPort.toString()].process.kill();
+    }
+}
+
+export const cleanUpFolder = () => { }
--- test/3.ipfs.data.test.ts
+++ test/3.ipfs.data.test.ts
@@ -1,5 +1,15 @@
+import { IPFSController, startIPFS, stopIPFS } from "@/lib/ipfs/index";
+
 describe('Test Publishing to IPFS', () => {
     test('Post Data', async () => {
-
+        const gatewayPort = 5001;
+        const apiPort = 9002;
+        let ipfsController: IPFSController = await startIPFS(gatewayPort, apiPort);
+        expect(ipfsController.gatewayPort).toEqual(gatewayPort);
+        expect(ipfsController.apiPort).toEqual(apiPort);
+        jest.setTimeout(120000);
+        let swarm = await ipfsController.api(`/swarm/peers`);
+        expect(swarm.Peers.length).toBeGreaterThan(0);
+        expect(await stopIPFS(5001)).toBe(true);
     })
 });
