c657700fcf4f0f7937ce1746a2b7d5762410a508
tj <tj@digitalarsenal.io>
2022-12-16 09:26:39 -0500

    update build

--- lib/routes/spacedata/get.ts
+++ lib/routes/spacedata/get.ts
@@ -1,53 +1,55 @@
 import { Request, Response } from "express";
 import * as express from "express";
 import * as standards from "@/lib/standards/standards";
-import standardsJSON from "@/lib/standards/schemas.json";
+import _standardsJSON from "@/lib/standards/schemas.json";
 import read from "@/lib/database/read";
 import { writeFB } from "@/lib/utility/flatbufferConversion";
 import { connection } from "@/lib/database/connection";
+import { KeyValueDataStructure } from "@/lib/class/utility/KeyValueDataStructure";
+
+const standardsJSON: KeyValueDataStructure = _standardsJSON;
 
 export const get: express.RequestHandler = async (req: Request, res: Response, next: Function) => {
 
-  if (!req.params.standard) {
-    res.send(Object.keys(standards));
+  if (req.params?.querytype?.split(".").pop() === 'map') {
+    res.end();
+    next();
   } else {
-    const standard = req.params.standard.toUpperCase();
-    let { query, format, schema } = req.query;
-    if (schema) {
-      //@ts-ignore
-      res.end(JSON.stringify(standardsJSON[standard], null, 4));
+    if (!req.params.standard) {
+      res.send(Object.keys(standards));
     } else {
-
-      try {
-        if (query) {
-          query = JSON.parse(query as string);
+      const standard = req.params.standard.toUpperCase();
+      let { query, format, schema } = req.query;
+      if (schema) {
+        res.end(JSON.stringify(standardsJSON[standard], null, 4));
+      } else {
+        let parsedQuery: Array<any> = [];
+        try {
+          if (query) {
+            parsedQuery = JSON.parse(query as string) as Array<any>;
+          }
+        } catch (e) {
+          console.log(e);
+          res.status(500);
+          res.end();
         }
-      } catch (e) {
-        console.log(e);
-        res.status(500);
-        res.end();
-      }
-      let payload;
-      if (req.params.querytype === "latest") {
+        let payload;
+        if (req.params.querytype === "latest") {
+          let { CID } = await connection("FILE_IMPORT_TABLE").select("CID").orderBy("updated_at").first();
+          parsedQuery.push(["where", ["file_id", "=", CID]]);
+        }
+        
+        payload = await read(connection, standard, standardsJSON[standard], (parsedQuery as Array<any>));
 
-        let { CID } = await connection("FILE_IMPORT_TABLE").select("CID").orderBy("updated_at").first();
-
-        //@ts-ignore
-        payload = await read(connection, standard, standardsJSON[standard], [
-          ["select", ["*"]],
-          ["where", ["file_id", "=", CID]]]);
-      } else {
-        //@ts-ignore
-        payload = await read(connection, standard, standardsJSON[standard], (query as Array<any>));
+        if (format === "json") {
+          payload = JSON.stringify(payload);
+        } else {
+          payload = writeFB(payload);
+        }
+        res.end(payload);
       }
-      if (format === "json") {
-        payload = JSON.stringify(payload);
-      } else {
-        payload = writeFB(payload);
-      }
-      res.end(payload);
     }
-  }
 
-  next();
+    next();
+  }
 };
