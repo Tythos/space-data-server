01540169bbbf3f8bc9ae96e89159f22df45b4200
tj <tj@digitalarsenal.io>
2022-12-21 12:36:33 -0500

    update build

--- lib/routes/spacedata/latest.ts
+++ lib/routes/spacedata/latest.ts
@@ -1,12 +1,8 @@
-import { Request, Response } from "express";
-import * as express from "express";
+
 import * as standards from "@/lib/standards/standards";
 import _standardsJSON from "@/lib/standards/schemas.json";
-import read from "@/lib/database/read";
-import { writeFB } from "@/lib/utility/flatbufferConversion";
-import { connection } from "@/lib/database/connection";
 import { KeyValueDataStructure } from "@/lib/class/utility/KeyValueDataStructure";
-import { createReadStream, existsSync, readFile, readFileSync } from "fs";
+import { createReadStream } from "fs";
 import { config } from "@/lib/config/config";
 import { join, resolve } from "path";
 
@@ -29,6 +25,7 @@ export const latest = async (req: any, res: any, next: any) => {
     res.end("ERROR: Invalid Format.");
   } else {
     const filePath = resolve(join(config.data.public, standard, provider, `latest.${format}`));
+    res.setHeader('Content-Type', `application/${format === "json" ? "json" : "octet-stream"}`);
 
     const fileStream = createReadStream(
       filePath
