599612548f8016726e75980aa52c5c9e24acd4e5
tj <tj@digitalarsenal.io>
2023-01-04 13:49:16 -0500

    update build

--- UI/src/App.svelte
+++ UI/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-col h-full w-full fixed bg-red-400">
+<div class="wrap-safe-pad flex flex-col h-full w-full fixed bg-black">
   <div class="flex justify-center items-center bg-blue-300">
     <div class="h-12 justify-center items-center flex w-full">
       Space Data Server
@@ -18,8 +18,11 @@
 
 <style global lang="postcss">
   .body-safe-pad {
+    overscroll-behavior: none;
     padding-left: env(safe-area-inset-left);
     padding-right: env(safe-area-inset-right);
+  }
+  .wrap-safe-pad {
     padding-top: env(safe-area-inset-top);
     padding-bottom: env(safe-area-inset-bottom);
   }
@@ -33,11 +36,12 @@
   :global(body, html) {
     overflow: hidden;
     min-height: 100vh;
+    height: 100vh;
+    overscroll-behavior: none;
     /* mobile viewport bug fix */
     min-height: -webkit-fill-available;
     min-width: -webkit-fill-available;
     width: 100vw;
-    height: 100vh;
     padding: 0px;
     margin: 0px;
   }
