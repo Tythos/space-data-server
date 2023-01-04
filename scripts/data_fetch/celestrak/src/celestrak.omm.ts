575cb1beb3ce93de772f6d65609e40f92284877a
tj <tj@digitalarsenal.io>
2023-01-04 15:35:43 -0500

    update build

--- scripts/data_fetch/celestrak/src/celestrak.omm.ts
+++ scripts/data_fetch/celestrak/src/celestrak.omm.ts
@@ -38,6 +38,7 @@ import { join } from "path";
     setTimeout(() => {
         rmSync(signatureFileName, { force: true, recursive: true });
         rmSync(fileName, { force: true, recursive: true });
+        console.log(signatureFileName, fileName);
     }, 1000);
 
 
