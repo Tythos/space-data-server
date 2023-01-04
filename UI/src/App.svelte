409f18ffe894065db31d56abeaf452ab36a612ba
tj <tj@digitalarsenal.io>
2023-01-04 13:17:09 -0500

    update build

--- UI/src/App.svelte
+++ UI/src/App.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-column h-full">
-  <div class="w-full fixed top-0 bg-white w-full z-10 h-12 bg-blue-300">
-    Header
+<div class="flex flex-col h-full fixed bg-red-400">
+  <div class="flex justify-center items-center bg-blue-300">
+    <div class="h-12 justify-center items-center flex w-full">
+      Space Data Server
+    </div>
   </div>
-  <div class="overflow-y-scroll fixed top-12 w-full">
-    <div class="bg-red-400 ">{new Array(3000).join("lorem ipsum")}</div>
+  <div class="overflow-y-scroll overflow-x-hidden break-all">
+    <div class="bg-red-400 ">
+      {new Array(3000).fill(0).map((n, i) => {
+        return i;
+      })}
+    </div>
   </div>
 </div>
 
 <style global lang="postcss">
+  @supports (-webkit-touch-callout: none) {
+    body {
+      /* The hack for Safari */
+      height: -webkit-fill-available;
+    }
+  }
   :global(body, html) {
     overflow: hidden;
+    min-height: 100vh;
+    /* mobile viewport bug fix */
+    min-height: -webkit-fill-available;
     width: 100vw;
     height: 100vh;
     padding: 0px;
     margin: 0px;
   }
-  
 </style>
