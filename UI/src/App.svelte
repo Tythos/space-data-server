6eec73388e3fb6e318710679180df11592391d6e
tj <tj@digitalarsenal.io>
2023-01-04 13:28:15 -0500

    update build

--- UI/src/App.svelte
+++ UI/src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-col h-full fixed bg-red-400">
+<div class="flex flex-col h-full w-full fixed bg-red-400">
   <div class="flex justify-center items-center bg-blue-300">
     <div class="h-12 justify-center items-center flex w-full">
       Space Data Server
     </div>
   </div>
-  <div class="overflow-y-scroll overflow-x-hidden break-all">
-    <div class="bg-red-400 ">
+  <div class="overflow-y-scroll overflow-x-hidden break-all body-safe-pad">
+    <div class="bg-red-400">
       {new Array(3000).fill(0).map((n, i) => {
         return i;
       })}
@@ -17,10 +17,17 @@
 </div>
 
 <style global lang="postcss">
+  .body-safe-pad {
+    padding-left: env(safe-area-inset-left);
+    padding-right: env(safe-area-inset-right);
+    padding-top: env(safe-area-inset-top);
+    padding-bottom: env(safe-area-inset-bottom);
+  }
   @supports (-webkit-touch-callout: none) {
     body {
       /* The hack for Safari */
       height: -webkit-fill-available;
+      width: -webkit-fill-available;
     }
   }
   :global(body, html) {
@@ -28,6 +35,7 @@
     min-height: 100vh;
     /* mobile viewport bug fix */
     min-height: -webkit-fill-available;
+    min-width: -webkit-fill-available;
     width: 100vw;
     height: 100vh;
     padding: 0px;
