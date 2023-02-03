7ad4ec191c79828a71e204065d640e6bbb44ec9b
tj <tj@digitalarsenal.io>
2023-02-03 12:23:50 -0500

    update build

--- UI/src/App.svelte
+++ UI/src/App.svelte
@@ -6,58 +6,62 @@
   import LoginModal from "./pages/User/LoginModal.svelte";
 </script>
 
-<nav
-  style="position:fixed;z-index:100"
-  class="font-sans navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
-  <div class="px-6 w-full flex flex-wrap items-center justify-between">
-    <div class="flex items-center">
-      <button
-        class="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContentY"
-        aria-controls="navbarSupportedContentY"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          class="w-5"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512">
-          <path
-            fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
-        </svg>
-      </button>
+<div
+  class="flex flex-col items-stretch h-full w-100vw fixed border-red-500 border-2 overflow-hidden m-0 p-0">
+  <nav
+    style="z-index:100"
+    class="w-100vw font-sans navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+    <div class="px-6 w-full flex flex-wrap items-center justify-between">
+      <div class="flex items-center">
+        <button
+          class="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContentY"
+          aria-controls="navbarSupportedContentY"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            class="w-5"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512">
+            <path
+              fill="currentColor"
+              d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
+          </svg>
+        </button>
+      </div>
+      <div
+        class="navbar-collapse collapse grow items-center"
+        id="navbarSupportedContentY">
+        <ul class="navbar-nav mr-auto min-[990px]:flex ">
+          {#each Object.entries(routes) as [route, value], i}
+            <li class="nav-item">
+              <a
+                class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#{route}"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light">{value.name}</a>
+            </li>
+          {/each}
+        </ul>
+      </div>
     </div>
-    <div
-      class="navbar-collapse collapse grow items-center"
-      id="navbarSupportedContentY">
-      <ul class="navbar-nav mr-auto min-[990px]:flex ">
-        {#each Object.entries(routes) as [route, value], i}
-          <li class="nav-item">
-            <a
-              class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
-              href="#{route}"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light">{value.name}</a>
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </div>
-  <div class="mr-6"><LoginModal /></div>
-</nav>
-<header>
-  <div class="font-sans text-center text-gray-800 py-20 px-6">
+    <div class="mr-6"><LoginModal /></div>
+  </nav>
+
+  <div
+    class="overflow-y-scroll font-sans text-center text-gray-800 py-20 px-6 h-full">
     <Router {routes} />
   </div>
-</header>
+</div>
+
 <style>
-  :global(.modal-backdrop){
+  :global(.modal-backdrop) {
     z-index: 0;
   }
 </style>
