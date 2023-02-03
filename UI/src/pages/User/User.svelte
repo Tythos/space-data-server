1d5008cc6f960aea685fca965dfc83ec3c6f9370
tj <tj@digitalarsenal.io>
2023-02-03 14:44:59 -0500

    update build

--- UI/src/pages/User/User.svelte
+++ UI/src/pages/User/User.svelte
@@ -1,2 +1,16 @@
-<script></script>
+<script>
+    import Web3 from "ethers";
+
+  // Check if ethereum is already injected by the browser (Mist/MetaMask)
+  /*let web3;
+  if (typeof window.ethereum !== 'undefined') {
+    // Use the browser's provider
+    web3 = new Web3(window.ethereum);
+  } else {
+    // Use a local provider
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }*/
+</script>
+
+
 <h1>USER LOGIN</h1>
