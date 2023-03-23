9c08671f076f34897ed5079e1c3dbc23a42d8e7a
tj <tj@digitalarsenal.io>
2023-03-23 11:08:14 -0400

    auth test

--- lib/routes/admin/index.ts
+++ lib/routes/admin/index.ts
@@ -1,35 +1,48 @@
-import { Request, Response } from "express";
-import * as express from "express";
+import { Request, Response, RequestHandler } from "express";
 import _standardsJSON from "@/lib/standards/schemas.json";
 import { KeyValueDataStructure } from "@/lib/class/utility/KeyValueDataStructure";
 import { config } from "@/lib/config/config";
 import { join, resolve } from "path";
 import * as ethers from "ethers";
 import { writeFileSync } from "node:fs";
+import { TrustedAddress } from "@/lib/class/settings.interface";
+import crypto from 'crypto';
 
 const standardsJSON: KeyValueDataStructure = _standardsJSON;
 const cFP = config?.data?.fileSystemPath;
 const fileReadPath = cFP && cFP[0] === "/" ?
     cFP : resolve(process.cwd(), cFP);
 
-export const getSettings: express.RequestHandler = async (req: Request, res: Response, next: Function) => {
-    res.json(config);
+export const getSettings: RequestHandler = async (req: Request, res: Response, next: Function) => {
+    //@ts-ignore
+    if (req.authHeader?.trustedAddress?.isAdmin) {
+        res.json(config);
+    } else {
+        res.status(401).json({ error: 'Unauthorized' });
+    }
 }
 
-export const cwd: express.RequestHandler = async (req: Request, res: Response, next: Function) => {
+export const cwd: RequestHandler = async (req: Request, res: Response, next: Function) => {
     res.json({ cwd: process.cwd() });
 }
 
-export const saveSettings: express.RequestHandler = async (req: Request, res: Response, next: Function) => {
-
-    const account = ethers.utils.verifyMessage(JSON.stringify(req.body), req.headers["authorization"] as any);
-    if (req.authHeader?.trustedAddress?.trust === 255) {
+export const saveSettings: RequestHandler = async (req: Request, res: Response, next: Function) => {
+    //@ts-ignore
+    if (req.authHeader?.trustedAddress?.isAdmin) {
         writeFileSync(join(process.cwd(), "config.json"), JSON.stringify(req.body, null, 4));
         setTimeout(() => {
             process.send?.("restartWorkers");
         }, 5000);
     }
-
-
 }
 
+export const adminCheck: RequestHandler = async (req: Request, res: Response, next: Function) => {
+    let admins = config.trustedAddresses
+        .filter((tA: TrustedAddress) => tA.isAdmin)
+        .map((tA: TrustedAddress) => {
+            const hasher = crypto.createHash('sha256');
+            hasher.update(tA.address);
+            return hasher.digest('hex');
+        });
+    res.json(admins);
+};
