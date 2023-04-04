da2a1a76b621914a4f2e3f1f695dbcb8e32643ac
tj <tj@digitalarsenal.io>
2023-04-04 19:31:38 -0400

    update build

--- lib/ipfs/pinfolder.ts
+++ lib/ipfs/pinfolder.ts
@@ -7,8 +7,25 @@ import chokidar from "chokidar";
 
 let debounce;
 
+const doPin = async (folderToPin, ipfsController) => {
+    console.log('Starting Pin');
+    if (existsSync(folderToPin)) {
+    } else {
+        console.log(`Folder ${folderToPin} does not exist, creating...`);
+        mkdirSync(folderToPin);
+    }
+    let CID = await ipfsController.publishDirectory(folderToPin);
+    console.log("Pinned Folder: ", CID);
+    await writeServerInfo({ ipfsCID: CID });
+    debounce = setTimeout(async () => {
+        await doPin(folderToPin, ipfsController);
+    }, 1000 * 60 * 60 * 3);
+}
+
 export const pinFolderInit = async (ipfsController) => {
     const folderToPin = resolve(__dirname, "..", config.data.fileSystemPath);
+
+    await doPin(folderToPin, ipfsController);
 
     return chokidar.watch(folderToPin, {
         awaitWriteFinish: {
@@ -20,15 +37,7 @@ export const pinFolderInit = async (ipfsController) => {
         clearTimeout(debounce);
 
         debounce = setTimeout(async () => {
-            console.log('Starting Pin');
-            if (existsSync(folderToPin)) {
-            } else {
-                console.log(`Folder ${folderToPin} does not exist, creating...`);
-                mkdirSync(folderToPin);
-            }
-            let CID = await ipfsController.publishDirectory(folderToPin);
-            console.log("Pinned Folder: ", CID);
-            await writeServerInfo({ ipfsCID: CID });
+            await doPin(folderToPin, ipfsController);
         }, 2000)
 
     })
