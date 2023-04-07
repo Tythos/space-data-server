f6b91dd338a3f422e6b2b03366d765f7319a98fb
tj <tj@digitalarsenal.io>
2023-04-07 12:03:07 -0400

    update build

--- UI/src/pages/User/LoginButton.svelte
+++ UI/src/pages/User/LoginButton.svelte
@@ -3,20 +3,21 @@
   import { push } from "svelte-spa-router";
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="text-gray-600 mr-2">
-  <div class="flex items-center">
-    {$ethWallet?.address
-      ? `${$ethWallet.address.slice(0, 5)}...${$ethWallet.address.slice(-5)}`
-      : ""}
+<div class="flex gap-2">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div class="text-gray-600 mr-2">
+    <div class="flex items-center justify-center h-full">
+      {$ethWallet?.address
+        ? `${$ethWallet.address.slice(0, 5)}...${$ethWallet.address.slice(-5)}`
+        : ""}
+    </div>
   </div>
-</div>
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div
-  on:click={(e) => {
-    push("/user");
-  }}
-  class="cursor-pointer
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div
+    on:click={(e) => {
+      push("/user");
+    }}
+    class="cursor-pointer
       px-5
       py-1
       h-12
@@ -26,5 +27,6 @@
       bg-gray-700
       text-white
       uppercase">
-  {$ethWallet?.address ? "Account" : "Login"}
+    {$ethWallet?.address ? "Account" : "Login"}
+  </div>
 </div>
